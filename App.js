c60783db1a34954583e34b327ae5498069e56df3
Mey-pix <meydrawsmii@gmail.com>
2024-11-11 09:21:05 -0300

    Front e Back

---
+++ App.js
@@ -0,0 +1,45 @@
+import { useState, createContext } from "react";
+import TelaCadastroProduto from "./componentes/Telas/TelaCadastroProduto";
+import TelaCadastroCategoria from "./componentes/Telas/TelaCadastroCategoria";
+import TelaMenu from "./componentes/Telas/TelaMenu";
+import Tela404 from "./componentes/Telas/Tela404";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TelalLogin from "./componentes/Telas/TelaLogin";
+
+export const ContextoUsuario = createContext();
+function App() {
+  const [usuario, setUsuario] = useState({
+    "usuario":"",
+    "logado":false
+  });
+
+  if(usuario.logado){
+    return(
+      <ContextoUsuario.Provider value={{usuario,setUsuario}}>
+        <TelalLogin/>
+      </ContextoUsuario.Provider>
+    );
+  }
+  else
+  {
+    return (
+      <div className="App">
+        <ContextoUsuario.Provider value={{usuario, setUsuario}}>
+        <BrowserRouter>
+          { //A ordem das rotas é importante 
+          }
+          <Routes>
+            <Route path="/produto" element={<TelaCadastroProduto />} />
+            <Route path="/categoria" element={<TelaCadastroCategoria />} />
+            <Route path="/login" element={<TelalLogin/>} />
+            <Route path="/" element={<TelaMenu />} />
+            <Route path="*" element={<Tela404 />} />
+          </Routes>
+        </BrowserRouter>
+        </ContextoUsuario.Provider>
+      </div>
+    );
+  }
+  } 
+
+export default App;
---
+++ dados/mockCategoria.js
@@ -0,0 +1,17 @@
+export const categorias = [
+    {
+        "id": 1,
+        "nome": "Livros",
+        "tipo": "Educação"
+    },
+    {
+        "id": 2,
+        "nome": "Ferramentas",
+        "tipo": "Mecanico"
+    },
+    {
+        "id": 3,
+        "nome": "Computador",
+        "tipo": "Tecnologia"
+    }
+]
